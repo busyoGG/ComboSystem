bfb558acca3d2b917043aef0546508f961764d98
BUSYO\buxiu <694253220@qq.com>
2023-11-01 23:32:51 +0800

    新增注释

--- Assets/ComboSystem/SkillTree.cs
+++ Assets/ComboSystem/SkillTree.cs
@@ -1,23 +1,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class SkillTree
 {
+    /// <summary>
+    /// 技能名称
+    /// </summary>
     public string name { get; set; }
+    /// <summary>
+    /// 技能id 一系列技能使用同一id
+    /// </summary>
     public int id { get; set; }
+    /// <summary>
+    /// 键位需求
+    /// </summary>
     public KeyCode key { get; set; }
+    /// <summary>
+    /// 下一连招列表
+    /// </summary>
     public List<SkillTree> next { get; set; }
+    /// <summary>
+    /// 技能持续时间
+    /// </summary>
     public float skillTime { get; set; }
+    /// <summary>
+    /// 是否正在释放技能
+    /// </summary>
     public bool isRunSkill { get; set; }
+    /// <summary>
+    /// 连招超时时间
+    /// </summary>
     public float outOfTime { get; set; }
+    /// <summary>
+    /// 是否可以强制打断技能
+    /// </summary>
     public bool isCanForceStop { get; set; }
+    /// <summary>
+    /// 强制打断次数
+    /// </summary>
     public int forceTimes { get; set; }
-    public SkillType skillType { get; set; }
-    public float holdingTime { get; set; }
+    /// <summary>
+    /// 默认强制打断次数
+    /// </summary>
     public int defaultForceTimes { get; set; }
-
+    /// <summary>
+    /// 强制打断重置时间
+    /// </summary>
     public float forceResetTime { get; set; }
+    /// <summary>
+    /// 技能类型
+    /// </summary>
+    public SkillType skillType { get; set; }
+    /// <summary>
+    /// 长按/蓄力时间
+    /// </summary>
+    public float holdingTime { get; set; }
+    /// <summary>
+    /// 进度条颜色
+    /// </summary>
     public Color progressColor { get; set; }
 
     public SkillTree() { 
